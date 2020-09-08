2182737363d04ff12c5906ba70d07299e78da47b
jchenTech <jchen98@163.com>
2020-09-08 15:33:20 +0800

    Finished lecture code for lecture 1

---
+++ lecture-code/intro1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers{
+    /** print the numbers until the loop stop */
+    public static void main(String[] args){
+        int x;
+        x = 0;
+        while (x < 10){
+            System.out.print(x + " ");
+            x = x + 1;
+        }
+    }
+}
