85001d43c8ebe76e6cad5cc4f7885055d9abc284
jchenTech <jchen98@163.com>
2020-09-08 15:26:47 +0800

    Deleted week123

--- lecture-code/week1/LEC2/DogLauncher.java
+++
@@ -1,8 +0,0 @@
-public class DogLauncher{
-	public static void main(String[] args) {
-		Dog d1 = new Dog(10);
-		Dog d2 = new Dog(100);
-	 	Dog bigger = Dog.maxDog(d1, d2);
-	 	bigger.makeNoise();
-	}
-}
